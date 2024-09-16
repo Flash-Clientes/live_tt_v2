fd0590d8d7c9907ca32868169e08b313bc9f2af6
kevin504-max <kevinbrito2012@flashvolve.com>
2024-09-16 09:40:34 -0300

    refact: make categories sidebar component with naiveui

--- src/views/dashboard/views/products-management/components/sidebar-categories/SidebarCategories.vue
+++ src/views/dashboard/views/products-management/components/sidebar-categories/SidebarCategories.vue
@@ -1,113 +1,75 @@
 <template>
-    <div class="sc-container">
-        <div class="sc-header">
-            <h3>Gerenciar Categorias</h3>
-            <!-- <button class="button button-primary">
-                <i class="fa fa-plus"></i> Adicionar Categoria
-            </button> -->
-        </div>
-
-        <div class="sc-body">
-            <InputSearch :placeholder="'Pesquisar categoria...'" />
-            <ul>
-                <li v-for="(category, index) in props.categories" :key="category.id">
-                    <input type="checkbox" :id="category.id" :name="category.id" :checked="index === 0">
-                    {{ category.name }} ({{ category.products }})
-                
-                    <div class="action-buttons">
-                        <button class="button button-secondary">
-                            <i class="fa fa-edit"></i>
-                        </button>
-                        <button class="button button-danger">
-                            <i class="fa fa-trash"></i>
-                        </button>
-                    </div>
-                </li>
-            </ul>     
-        </div>
-
-        <div class="sc-footer">
-            <button class="button button-primary">
-                <i class="fa fa-plus"></i> Adicionar Categoria
-            </button>
-        </div>
+  <div class="sc-container">
+    <div class="sc-header">
+      <n-h3>Gerenciar Categorias</n-h3>
     </div>
+
+    <div class="sc-body">
+      <InputSearch :placeholder="'Pesquisar categoria...'" />
+      <n-list bordered>
+        <n-list-item v-for="(category, index) in props.categories" :key="category.id">
+          <n-checkbox :checked="index === 0">
+            {{ category.name }} ({{ category.products }})
+          </n-checkbox>
+          <div class="action-buttons">
+            <n-button text size="small" type="primary">
+              <n-icon><i class="fa fa-edit"></i></n-icon>
+            </n-button>
+            <n-button text size="small" type="error">
+              <n-icon><i class="fa fa-trash"></i></n-icon>
+            </n-button>
+          </div>
+        </n-list-item>
+      </n-list>
+    </div>
+
+    <div class="sc-footer">
+      <n-button type="primary" size="small">
+        <n-icon><i class="fa fa-plus"></i></n-icon> Adicionar Categoria
+      </n-button>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import { defineProps } from 'vue';
+import { NList, NListItem, NButton, NIcon, NCheckbox, NH3 } from 'naive-ui';
 import InputSearch from '../input-search/InputSearch.vue';
 
-const props = defineProps([
-    'categories',
-]);
+const props = defineProps(['categories']);
 </script>
 
 <style scoped>
 .sc-container {
-    display: flex;
-    flex-direction: column;
-    width: 290px;
-    background-color: #f5f5f5;
+  display: flex;
+  flex-direction: column;
+  width: 290px;
+  background-color: #f5f5f5;
 }
 
-.sc-container .sc-header {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    padding: 0 10px;
+.sc-header {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 0 10px;
 }
 
 .sc-body {
-    padding: 10px;
+  padding: 10px;
 }
 
-.sc-body ul {
-    list-style: none;
-    padding: 0;
-}
-
-.sc-body ul li {
-    display: flex;
-    justify-content: flex-start;
-    align-items: flex-end;
-    padding: 10px;
-    cursor: pointer;
-}
-
-.sc-body ul li:hover {
-    background-color: #e0e0e0;
-}
-
-.sc-body ul li input {
-    margin-right: 10px;
-}
-
-.sc-body ul li input[type="checkbox"] {
-    cursor: pointer;
-}
-
-.sc-body ul li .action-buttons {
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-    gap: 5px;
-    margin-left: auto;
+.action-buttons {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 5px;
+  margin-left: auto;
 }
 
 .sc-footer {
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-    padding: 10px;
-}
-
-.sc-footer button {
-    font-size: 12px;
-}
-
-button {
-    font-size: 10px;
-    padding: 5px 10px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 10px;
 }
 </style>
